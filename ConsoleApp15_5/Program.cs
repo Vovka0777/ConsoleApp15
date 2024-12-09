525289bae653ecf84b0720b744a699c3fce5e661
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2024-12-09 19:40:38 +0200

    Add files via upload

---
+++ ConsoleApp15_5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Задача 5. В массиве хранятся сведения об оценках 25 учеников по химии. Определить количество неуспевающих по химии учеников.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int size = Size();
+            int[] array = Inp(size);
+            Out(array);
+            Console.WriteLine($"\nНеуспевающие:");
+            Unspecified(array);
+            Console.ReadLine();
+        }
+        static int Size()
+        {
+            int[] array = new int[25];
+            return 25;
+        }
+        static int[] Inp(int size)
+        {
+            int[] array = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(2, 6);
+            }
+            return array;
+        }
+
+        static int Unspecified(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] <= 3)
+                {
+                    Console.WriteLine($"Ученик[{i}]= {array[i]}");
+                    count++;
+                }
+            }
+            return count;
+
+        }
+        static void Out(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Ученик[{i}]= {array[i]}");
+            }
+        }
+    }
+}
