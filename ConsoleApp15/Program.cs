df3da456a6a3c09747a05c94712a4c6feb7264e4
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2024-12-09 19:33:55 +0200

    Add files via upload

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Задача 1. Массив предназначен для хранения значений ростов двенадцати человек.
+        /// С помощью датчика случайных чисел заполнить массив целыми значениями, лежащими в диапазоне от 163 до 190 включительно.
+        /// Вывести значения элементов с использованием цикла for. Найти средний рост.
+        /// </summary>
+        /// <param name="args"></param>
+        static int[] Inp(int size)
+        {
+            int[] array = new int[size];
+        Random rnd = new Random();
+            for (int i = 0; i<array.Length; i++)
+            {
+                array[i] = rnd.Next(163, 191);
+
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            int size = Size();
+            int[] array = Inp(size);
+            OutPut(array);
+            Console.ReadLine();
+        }
+        static int Size()
+        {
+            int[] array = new int[12];
+            return 12;
+        }
+        static void OutPut(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"array[{i}]= {array[i]}");
+            }
+            Console.WriteLine(Average(array));
+        }
+        static double Average(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum / array.Length;
+        }
+    }
+}
---
+++ ConsoleApp15/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp15")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp15")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4c1d3e9e-51a3-4e3a-9daf-27e369499338")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
